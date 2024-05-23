17ea93c86401d61af512b4506b55dd42478d2f5a
nekottyo <nekottyo@gmail.com>
2024-05-23 16:38:40 +0900

    feat: update for rev8

--- keyboards/keebio/iris/keymaps/nekottyo/keymap.c
+++ keyboards/keebio/iris/keymaps/nekottyo/keymap.c
@@ -1,9 +1,7 @@
-#include "iris.h"
-#include "keymap_swedish.h"
-#include "action_layer.h"
-#include "eeconfig.h"
+// Copyright 2023 Danny Nguyen (@nooges)
+// SPDX-License-Identifier: GPL-2.0-or-later
 
-extern keymap_config_t keymap_config;
+#include QMK_KEYBOARD_H
 
 #define _QWERTY 0
 #define _LOWER 1
@@ -39,27 +37,27 @@ enum custom_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-  [_QWERTY] = LAYOUT_kc(
-  //,----+----+----+----+----+----.              ,----+----+----+----+----+----.
-     GRV , 1  , 2  , 3  , 4  , 5  ,                6  , 7  , 8  , 9  , 0  ,BSPC,
-  //|----+----+----+----+----+----|              |----+----+----+----+----+----|
-     TAB , Q  , W  , E  , R  , T  ,                Y  , U  , I  , O  , P  ,BSLS,
-  //|----+----+----+----+----+----|              |----+----+----+----+----+----|
-     LCTL, A  , S  , D  , F  , G  ,                H  , J  , K  , L  , OE , AE ,
-  //|----+----+----+----+----+----+----.    ,----|----+----+----+----+----+----|
-     LSFT, Z  , X  , C  , V  , B  ,ESC ,     RCTL, N  , M  ,COMM,DOT ,SLSH,RSFT,
-  //`----+----+----+--+-+----+----+----/    \----+----+----+----+----+----+----'
-                       LGUI,LOWR,SPC ,         ENT ,RASE,LALT
-  //                  `----+----+----'        `----+----+----'
+  [_QWERTY] = LAYOUT(
+  //,-------+-------+-------+-------+-------+-------.                    ,-------+-------+-------+-------+-------+-------.
+     KC_GRV ,KC_1   ,KC_2   ,KC_3   ,KC_4   ,KC_5   ,                     KC_6   ,KC_7   ,KC_8   ,KC_9   ,KC_0   ,KC_BSPC,
+  //|-------+-------+-------+-------+-------+-------|                    |-------+-------+-------+-------+-------+-------|
+     KC_TAB ,KC_Q   ,KC_W   ,KC_E   ,KC_R   ,KC_T   ,                     KC_Y   ,KC_U   ,KC_I   ,KC_O   ,KC_P   ,KC_BSLS,
+  //|-------+-------+-------+-------+-------+-------|                    |-------+-------+-------+-------+-------+-------|
+     KC_LCTL,KC_A   ,KC_S   ,KC_D   ,KC_F   ,KC_G   ,                     KC_H   ,KC_J   ,KC_K   ,KC_L   ,KC_SCLN,KC_QUOT,
+  //|-------+-------+-------+-------+-------+-------+-------.    ,-------|-------+-------+-------+-------+-------+-------|
+     KC_LSFT,KC_Z   ,KC_X   ,KC_C   ,KC_V   ,KC_B   ,KC_ESC,     KC_RCTL ,KC_N   ,KC_M   ,KC_COMM,KC_DOT ,KC_SLSH,KC_RSFT,
+  //`-------+-------+-------+--+----+-------+-------+-------/    \-------+-------+-------+-------+-------+-------+-------'
+                                   KC_LGUI,LOWER  ,KC_SPC ,         KC_ENT ,RAISE ,KC_LALT
+  //                              `-------+-------+-------'        `-------+-------+-------'
   ),
 
   [_LOWER] = LAYOUT(
   //,-------+-------+-------+-------+-------+-------.                    ,-------+-------+-------+-------+-------+-------.
      KC_TILD,KC_EXLM,KC_AT  ,KC_HASH,KC_DLR ,KC_PERC,                     KC_CIRC,KC_AMPR,KC_ASTR,KC_SLSH,KC_LPRN,KC_RPRN,
   //|-------+-------+-------+-------+-------+-------|                    |-------+-------+-------+-------+-------+-------|
-     KC_TRNS,M_B2   ,M_U    ,M_B1   ,M_WHU  ,KC_TRNS,                     M_PASTE,KC_TRNS,KC_TRNS,KC_PIPE,KC_LCBR,KC_RCBR,
+     QK_BOOT,M_B2   ,M_U    ,M_B1   ,M_WHU  ,KC_TRNS,                     M_PASTE,KC_TRNS,KC_TRNS,KC_PIPE,KC_LCBR,KC_RCBR,
   //|-------+-------+-------+-------+-------+-------|                    |-------+-------+-------+-------+-------+-------|
-     KC_TRNS,M_L    ,M_D    ,M_R    ,M_WHD  ,KC_BSLS,                     KC_LEFT,KC_DOWN,KC_UP  ,KC_RGHT,KC_LBRC,KC_RBRC,
+     BL_STEP,M_L    ,M_D    ,M_R    ,M_WHD  ,KC_BSLS,                     KC_LEFT,KC_DOWN,KC_UP  ,KC_RGHT,KC_LBRC,KC_RBRC,
   //|-------+-------+-------+-------+-------+-------+-------.    ,-------|-------+-------+-------+-------+-------+-------|
      KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,     KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,
   //`-------+-------+-------+--+----+-------+-------+-------/    \-------+-------+-------+-------+-------+-------+-------'
@@ -71,11 +69,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-------+-------+-------+-------+-------+-------.                    ,-------+-------+-------+-------+-------+-------.
      KC_F12 ,KC_F1  ,KC_F2  ,KC_F3  ,KC_F4  ,KC_F5  ,                     KC_F6  ,KC_F7  ,KC_F8  ,KC_F9  ,KC_F10 ,KC_F11 ,
   //|-------+-------+-------+-------+-------+-------|                    |-------+-------+-------+-------+-------+-------|
-     KC_TRNS,KC_TRNS,KC_TRNS,KC_EQL,KC_MINS,KC_ASTR,                     KC_COLON,KC_UNDS,KC_TRNS,KC_TRNS,KC_TRNS,KC_PIPE,
+     RGB_TOG,RGB_HUI,RGB_HUD,KC_EQL ,KC_MINS,KC_ASTR,                     KC_COLON,KC_UNDS,KC_TRNS,KC_TRNS,KC_TRNS,KC_PIPE,
   //|-------+-------+-------+-------+-------+-------|                    |-------+-------+-------+-------+-------+-------|
-     KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_PLUS,KC_SLSH,                     KC_HOME,KC_PGDN,KC_PGUP,KC_END ,KC_COLON,KC_BSLS,
+     RGB_MOD,RGB_SAI,RGB_SAD,KC_TRNS,KC_PLUS,KC_SLSH,                     KC_HOME,KC_PGDN,KC_PGUP,KC_END ,KC_COLON,KC_BSLS,
   //|-------+-------+-------+-------+-------+-------+-------.    ,-------|-------+-------+-------+-------+-------+-------|
-     KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_EQL ,KC_TRNS,     KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,
+     KC_MUTE,RGB_VAI,RGB_VAD,KC_TRNS,KC_TRNS,KC_EQL ,KC_TRNS,     KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,
   //`-------+-------+-------+--+----+-------+-------+-------/    \-------+-------+-------+-------+-------+-------+-------'
                                    KC_TRNS,KC_NO  ,KC_TRNS,         KC_TRNS,KC_TRNS,KC_TRNS
   //                              `-------+-------+-------'        `-------+-------+-------'
@@ -91,7 +89,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
     case QWERTY:
       if (record->event.pressed) {
-        persistent_default_layer_set(1UL<<_QWERTY);
+        set_single_persistent_default_layer(_QWERTY);
       }
       return false;
       break;
